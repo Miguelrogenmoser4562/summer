0ae0ac04ec6067d65b53db4ff915c4011fdbbffd
root <root@DESKTOP-90S0D38.localdomain>
2022-12-26 17:48:20 -0600

    LOTS OF STUFF HERE

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    username = db.Column(db.String(), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
 
 @app.route("/")
 def home():
@@ -19,3 +25,16 @@ def login():
         return redirect("/")
     else:
         return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        if session.get("user_id") != None:
+            return redirect("/")
+        return render_template("register.html")
+    else:
+        phone_number = request.form["phone_number"]
+        person = User(username=phone_number)
+        db.session.add(person)
+        db.session.commit()
+        return render_template("dummy.html", phone_number=phone_number)
