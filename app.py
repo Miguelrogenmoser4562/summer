97c981ea271f04dba8af413cb063d27d9c4274fd
root <root@DESKTOP-90S0D38.localdomain>
2022-12-31 16:38:13 -0600

    computer change

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     username = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(), nullable=False)
     id = db.Column(db.Integer, primary_key=True)
 
 @app.route("/")
@@ -33,8 +35,21 @@ def register():
             return redirect("/")
         return render_template("register.html")
     else:
-        phone_number = request.form["phone_number"]
-        person = User(username=phone_number)
-        db.session.add(person)
-        db.session.commit()
-        return render_template("dummy.html", phone_number=phone_number)
+        if int(request.form['Test']) == 1:
+            if request.form["Email"] == "" or request.form['Password'] == "":
+                return render_template("register.html", error="Email or password missing")
+            Email = request.form["Email"]
+            Password = request.form['Password']
+            person = User(email=Email, password=Password, username="Default" + str(db.session.query(User).count()))
+            db.session.add(person)
+            db.session.commit()
+            return render_template("dummy.html", error="", id=str(db.session.query(User.id).filter(User.email == Email, User.password == Password).all()[0].id))
+        else:
+            if db.session.query(User).filter(User.username == request.form['Username']).count() >= 1:
+                return render_template("dummy.html", error="Username already taken.")
+            if len(request.form['Username']) > 60:
+                return render_template("dummy.html", error="Please pick a shorter name")
+            user = db.session.query(User).filter(User.id == int(request.form["id"])).all()[0]
+            user.username = request.form["Username"]
+            db.session.commit()
+            return "all good!"
